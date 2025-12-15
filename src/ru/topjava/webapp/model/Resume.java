25cb5791adcf5d516e499cf5065a609fa0a76ae8
Евгений <Antonov.174.rus@mail.ru>
2025-12-15 15:40:40 +0500

    Добавил методы update, findIndexByUuid. Убрал дублирование кода в ArrayStorage.

--- src/ru/topjava/webapp/model/Resume.java
+++ src/ru/topjava/webapp/model/Resume.java
@@ -1,15 +1,20 @@
+package ru.topjava.webapp.model;
+
 import java.util.Objects;
 
 /**
  * Initial resume class
  */
 public class Resume {
-
     // Unique identifier
-    String uuid;
+    private String uuid;
 
     public String getUuid() {
         return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 
     @Override
---
+++ src/ru/topjava/webapp/storage/ArrayStorage.java
@@ -0,0 +1,68 @@
+package ru.topjava.webapp.storage;
+
+import ru.topjava.webapp.model.Resume;
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private static final int CAPACITY = 10000;
+    private Resume[] storage = new Resume[CAPACITY];
+    private int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int targetIndex = findIndexByUuid(r.getUuid());
+        storage[targetIndex] = r;
+    }
+
+    public void save(Resume r) {
+        if (size >= CAPACITY) {
+            throw new IllegalArgumentException("Массив данных переполнен");
+        }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(r)) {
+                throw new IllegalArgumentException(
+                        String.format("Резюме с uuid %s уже есть в базе", r.getUuid()));
+            }
+        }
+        storage[size++] = r;
+    }
+
+    public Resume get(String uuid) {
+        int targetIndex = findIndexByUuid(uuid);
+        return storage[targetIndex];
+    }
+
+    public void delete(String uuid) {
+        int indexForDel = findIndexByUuid(uuid);
+        storage[indexForDel] = storage[size - 1];
+        storage[--size] = null;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int findIndexByUuid(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException(
+                String.format("Резюме с uuid %s не найдено", uuid));
+    }
+}
