a1f33c43bccca1f6904c4288b73fb82db745dd65
Евгений <Antonov.174.rus@mail.ru>
2025-12-19 22:14:52 +0500

    hw_3: make core storage methods final

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -11,34 +11,34 @@ public abstract class AbstractArrayStorage implements Storage {
 
     protected abstract int findIndex(String uuid);
 
-    public void clear() {
+    public final void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void update(Resume r) {
+    public final void update(Resume r) {
         int targetIndex = findIndex(r.getUuid());
         storage[targetIndex] = r;
     }
 
     public abstract void save(Resume r);
 
-    public Resume get(String uuid) {
+    public final Resume get(String uuid) {
         int targetIndex = findIndex(uuid);
         return storage[targetIndex];
     }
 
-    public void delete(String uuid) {
+    public final void delete(String uuid) {
         int indexForDel = findIndex(uuid);
         storage[indexForDel] = storage[size - 1];
         storage[--size] = null;
     }
 
-    public Resume[] getAll() {
+    public final Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    public int size() {
+    public final int size() {
         return size;
     }
 }
