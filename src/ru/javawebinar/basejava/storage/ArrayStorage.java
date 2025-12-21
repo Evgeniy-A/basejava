769af91433861cc004febd5de52d2c9c6083c649
Евгений <Antonov.174.rus@mail.ru>
2025-12-21 17:54:40 +0500

    hw_3: apply template method to save in AbstractArrayStorage
    - refactor AbstractArrayStorage to use template method for saving
    items.
    - implement saving logic in abstract methods and streamline code.

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -3,26 +3,25 @@ package ru.javawebinar.basejava.storage;
 import ru.javawebinar.basejava.model.Resume;
 
 public class ArrayStorage extends AbstractArrayStorage {
-    public void save(Resume r) {
-        if (size >= CAPACITY) {
-            throw new IllegalArgumentException("Массив данных переполнен");
-        }
-        for (int i = 0; i < size; i++) {
-            if (storage[i].equals(r)) {
-                throw new IllegalArgumentException(
-                        String.format("Резюме с uuid %s уже есть в базе", r.getUuid()));
-            }
-        }
-        storage[size++] = r;
-    }
-
+    @Override
     protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
         }
-        throw new IllegalArgumentException(
-                String.format("Резюме с uuid %s не найдено", uuid));
+        return -1;
+    }
+
+    @Override
+    public void addResume(Resume r, int index) {
+        index = size;
+        storage[index] = r;
+        size++;
+    }
+
+    @Override
+    public void removeAt(int index) {
+        storage[index] = storage[size - 1];
     }
 }
