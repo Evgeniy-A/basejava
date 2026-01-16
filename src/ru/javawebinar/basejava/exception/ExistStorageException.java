29779774214fd2cbed1ae8a6baa2d0eada3fddf1
Евгений <Antonov.174.rus@mail.ru>
2026-01-16 18:04:23 +0500

    hw_4: fix issues and clean up code

--- src/ru/javawebinar/basejava/exception/ExistStorageException.java
+++ src/ru/javawebinar/basejava/exception/ExistStorageException.java
@@ -2,11 +2,6 @@ package ru.javawebinar.basejava.exception;
 
 public class ExistStorageException extends StorageException {
     public ExistStorageException(String uuid) {
-        super(uuid);
-    }
-
-    @Override
-    public String getMessage() {
-        return "Резюме с UUID: " + getUuid() + " уже есть в базе";
+        super("Резюме с uuid: " + uuid + " уже есть в базе", uuid);
     }
 }
