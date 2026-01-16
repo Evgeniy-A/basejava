24d4c9c4f5fa1fc947d53335a212bcce8bb11b6f
Евгений <Antonov.174.rus@mail.ru>
2026-01-16 18:04:23 +0500

    Поправил код

--- src/ru/javawebinar/basejava/exception/NotExistStorageException.java
+++ src/ru/javawebinar/basejava/exception/NotExistStorageException.java
@@ -2,11 +2,6 @@ package ru.javawebinar.basejava.exception;
 
 public class NotExistStorageException extends StorageException {
     public NotExistStorageException(String uuid) {
-        super(uuid);
-    }
-
-    @Override
-    public String getMessage() {
-        return "Резюме с UUID " + getUuid() + " нет в базе";
+        super("Резюме с uuid: " + uuid + " нет в базе", uuid);
     }
 }
