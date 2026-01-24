18228cd20cac3056be1ad556510937d973690c59
Евгений <Antonov.174.rus@mail.ru>
2026-01-24 22:44:35 +0500

    hw_5: refactor code for improvements

--- test/ru/javawebinar/basejava/storage/junit5/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/junit5/ArrayStorageTest.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.basejava.storage.junit5;
 
 import ru.javawebinar.basejava.storage.ArrayStorage;
-import ru.javawebinar.basejava.storage.junit4.AbstractArrayStorageTest;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest {
     public ArrayStorageTest() {
