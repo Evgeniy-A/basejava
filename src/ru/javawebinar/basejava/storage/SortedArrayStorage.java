c3253a47b4e1ceeda357ecc67dd5d101ab95b452
Евгений <Antonov.174.rus@mail.ru>
2026-01-24 22:32:47 +0500

    hw_5: refactor storage: add AbstractStorage and move common logic

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -6,21 +6,23 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
-    protected int findIndex(String uuid) {
+    protected Integer findSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 
     @Override
-    public void addResume(Resume r, int index) {
+    public void insertResume(Resume r, int index) {
         index = -index - 1;
         System.arraycopy(storage, index, storage, index + 1, size - index);
         storage[index] = r;
     }
 
     @Override
-    public void removeResume(int index) {
+    public void removeResume(Object searchKey) {
+        int index = (int) searchKey;
         int countMoved = size - index - 1;
         System.arraycopy(storage, index + 1, storage, index, countMoved);
+        deleteLast();
     }
 }
