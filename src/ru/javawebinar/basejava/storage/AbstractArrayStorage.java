d71a25459be02cb50a9bcc5bae2d8b3e5df6e81e
Евгений <Antonov.174.rus@mail.ru>
2026-02-08 14:25:16 +0500

    hw_3: implement template methods; maintain sorted order in SortedArrayStorage via binarySearch

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int CAPACITY = 10000;
     protected Resume[] storage = new Resume[CAPACITY];
@@ -9,9 +11,31 @@ public abstract class AbstractArrayStorage implements Storage {
 
     protected abstract int findIndex(String uuid);
 
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int targetIndex = findIndex(r.getUuid());
+        storage[targetIndex] = r;
+    }
+
+    public abstract void save(Resume r);
+
     public Resume get(String uuid) {
         int targetIndex = findIndex(uuid);
         return storage[targetIndex];
+    }
+
+    public void delete(String uuid) {
+        int indexForDel = findIndex(uuid);
+        storage[indexForDel] = storage[size - 1];
+        storage[--size] = null;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
