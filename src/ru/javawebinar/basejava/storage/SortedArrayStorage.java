9edd118071b3c095850d94dde52c8aed92034ab3
Евгений <Antonov.174.rus@mail.ru>
2026-02-16 21:07:02 +0500

    lesson_6: replace anonymous Comparator with Comparator.comparing
    - refactor SortedArrayStorage to use Comparator.comparing instead
    of anonymous Comparator.
    - simplify sorting logic and improve code readability.

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -6,17 +6,11 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<>() {
-        @Override
-        public int compare(Resume o1, Resume o2) {
-            return o1.getUuid().compareTo(o2.getUuid());
-        }
-    };
 
     @Override
     protected Integer findSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
+        return Arrays.binarySearch(storage, 0, size, searchKey, Comparator.comparing(Resume::getUuid));
     }
 
     @Override
