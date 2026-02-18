1b24ac401b8b43d574fcba3baa8f9553df3454ca
Евгений <Antonov.174.rus@mail.ru>
2026-02-18 19:18:12 +0500

    hw_6: refactor Resume with fullName and implement Comparable
    - add fullName field to Resume and implement Comparable using
    fullName and uuid.
    - introduce a factory method for creating Resume instances.

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -3,19 +3,39 @@ package ru.javawebinar.basejava.model;
 import java.util.Objects;
 import java.util.UUID;
 
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.nullsFirst;
+
 /**
  * Initial resume class
  */
-public class Resume {
-    // Unique identifier
+public class Resume implements Comparable<Resume> {
+    private String fullName;
     private final String uuid;
 
     public Resume() {
-        this(UUID.randomUUID().toString());
+        this.uuid = UUID.randomUUID().toString();
     }
 
-    public Resume(String uuid) {
+    private Resume(String uuid, String fullName) {
         this.uuid = uuid;
+        this.fullName = fullName;
+    }
+
+    public static Resume ofUuid(String uuid) {
+        return new Resume(uuid, null);
+    }
+
+    public static Resume ofFullName(String fullName) {
+        return new Resume(UUID.randomUUID().toString(), fullName);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getUuid() {
@@ -37,5 +57,12 @@ public class Resume {
     @Override
     public int hashCode() {
         return Objects.hashCode(uuid);
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return comparing(Resume::getFullName, nullsFirst(String::compareTo))
+                .thenComparing(Resume::getUuid)
+                .compare(this, o);
     }
 }
