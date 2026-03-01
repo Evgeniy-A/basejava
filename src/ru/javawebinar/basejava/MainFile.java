c7dbb57bc22aa7119030a90dabc8e451c74c3205
Евгений <Antonov.174.rus@mail.ru>
2026-03-01 19:24:25 +0500

    hw_8: implement recursive traversal to print file names in project root

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,33 +1,26 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) {
-        String filePath = ".\\.gitignore";
+        File dir = new File("./src/ru");
+        printFilesRecursively(dir);
 
-        File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
+    }
+
+    public static void printFilesRecursively(File dir) {
+        File[] files = dir.listFiles();
+        if (files == null) {
+            return;
         }
-
-        File dir = new File("./src/ru/javawebinar/basejava");
-        System.out.println(dir.isDirectory());
-        String[] list = dir.list();
-        if (list != null) {
-            for (String name : list) {
-                System.out.println(name);
+        for (File file : files) {
+            if (file.isDirectory()) {
+                System.out.println(file.getName().toUpperCase());
+                printFilesRecursively(file);
+            } else {
+                System.out.println(file.getName());
             }
-        }
-
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            System.out.println(fis.read());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
