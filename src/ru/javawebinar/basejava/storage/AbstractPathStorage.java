2542030eb42b45aa795c37e6ce066f8bc9f1400c
Евгений <Antonov.174.rus@mail.ru>
2026-03-11 20:12:29 +0500

    lesson_9: implement AbstractPathStorage

---
+++ src/ru/javawebinar/basejava/storage/AbstractPathStorage.java
@@ -0,0 +1,100 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public abstract class AbstractPathStorage extends AbstractStorage<Path> {
+    private final Path directory;
+
+    protected AbstractPathStorage(String dir) {
+        directory = Paths.get(dir);
+        if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
+            throw new IllegalArgumentException(dir + " is not directory");
+        }
+    }
+
+    protected abstract void writeResume(Resume r, OutputStream os) throws IOException;
+
+    protected abstract Resume readResume(InputStream is) throws IOException;
+
+    @Override
+    protected Path findSearchKey(String uuid) {
+        return directory.resolve(uuid);
+    }
+
+    @Override
+    protected boolean isExist(Path path) {
+        return Files.exists(path);
+    }
+
+    @Override
+    protected void replaceResume(Resume r, Path path) {
+        try {
+            writeResume(r, new BufferedOutputStream(Files.newOutputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("IO error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void addResume(Resume r, Path path) {
+        try {
+            writeResume(r, new BufferedOutputStream(Files.newOutputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("IO error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void removeResume(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("Delete Resume error", directory.toAbsolutePath().toString(), e);
+        }
+    }
+
+    @Override
+    protected Resume getResume(Path path) {
+        try {
+            return readResume(new BufferedInputStream(Files.newInputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("Path read error", directory.toAbsolutePath().toString(), e);
+        }
+    }
+
+    @Override
+    protected List<Resume> getResumesAsList() {
+        List<Resume> list = new ArrayList<>();
+        for (Path path : getAllFiles()) {
+            list.add(getResume(path));
+        }
+        return list;
+    }
+
+    @Override
+    public void clear() {
+        getAllFiles().forEach(this::removeResume);
+    }
+
+    @Override
+    public int size() {
+        return getAllFiles().size();
+    }
+
+    private List<Path> getAllFiles() {
+        try (Stream<Path> stream = Files.list(directory)) {
+            return stream.toList();
+        } catch (IOException e) {
+            throw new StorageException("Path error", directory.toAbsolutePath().toString());
+        }
+    }
+}
