c1f4cd977927403eca62b1f24d1e2dfd16bfb61b
Евгений <Antonov.174.rus@mail.ru>
2026-03-12 16:52:13 +0500

    hw_9: add directory tree printer with sorted output and visual indentation

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,25 +1,35 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class MainFile {
     public static void main(String[] args) {
         File dir = new File("./src/ru");
-        printDirectoryTree(dir);
+        printDirectoryTree(dir, "");
 
     }
 
-    public static void printDirectoryTree(File dir) {
+    public static void printDirectoryTree(File dir, String indent) {
         File[] files = dir.listFiles();
         if (files == null) {
             return;
         }
-        for (File file : files) {
-            if (file.isDirectory()) {
-                System.out.println(file.getName().toUpperCase());
-                printDirectoryTree(file);
-            } else {
-                System.out.println(file.getName());
+        Arrays.sort(files, (f1, f2) -> {
+            if (f1.isDirectory() && !f2.isDirectory()) return -1;
+            if (!f1.isDirectory() && f2.isDirectory()) return 1;
+            return f1.getName().compareToIgnoreCase(f2.getName());
+        });
+
+        for (int i = 0; i < files.length; i++) {
+            boolean isLast = (i == files.length - 1);
+            String connector = isLast ? "|__ " : "|-- ";
+            String nextIndent = isLast ? "    " : "|   ";
+
+            System.out.println(indent + connector + files[i].getName());
+
+            if (files[i].isDirectory()) {
+                printDirectoryTree(files[i], indent + nextIndent);
             }
         }
     }
