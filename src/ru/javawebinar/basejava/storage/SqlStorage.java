eda057c9b29a3659a860705cd503656c8497992b
Евгений <Antonov.174.rus@mail.ru>
2026-04-10 21:25:33 +0500

    hw_13: implement all CRUD methods in SqlStorage

--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -1,73 +1,88 @@
 package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.exception.NotExistStorageException;
-import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
-import ru.javawebinar.basejava.sql.ConnectionFactory;
+import ru.javawebinar.basejava.sql.SqlHelper;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SqlStorage implements Storage {
-    public final ConnectionFactory connectionFactory;
+    public final SqlHelper sqlHelper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
-        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
     @Override
     public void clear() {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("DELETE FROM resume")) {
-            ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        sqlHelper.runSQL("DELETE FROM resume");
     }
 
     @Override
     public void update(Resume r) {
+        sqlHelper.runSQL("UPDATE resume  SET full_name = ? WHERE uuid = ?", pst -> {
+            pst.setString(1, r.getFullName());
+            pst.setString(2, r.getUuid());
+            if (pst.executeUpdate() == 0) {
+                throw new NotExistStorageException(r.getUuid());
+            }
+            return null;
+        });
     }
 
     @Override
     public void save(Resume r) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?, ?)")) {
-            ps.setString(1, r.getUuid());
-            ps.setString(2, r.getFullName());
-            ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        sqlHelper.runSQL("INSERT INTO resume (uuid, full_name) VALUES (?, ?)", pst -> {
+            pst.setString(1, r.getUuid());
+            pst.setString(2, r.getFullName());
+            pst.execute();
+            return null;
+        });
     }
 
     @Override
     public Resume get(String uuid) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid =?")) {
-            ps.setString(1, uuid);
-            ResultSet rs = ps.executeQuery();
+        return sqlHelper.runSQL("SELECT * FROM resume r WHERE r.uuid =?", pst -> {
+            pst.setString(1, uuid);
+            ResultSet rs = pst.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
             return Resume.of(uuid, rs.getString("full_name"));
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
+        });
     }
 
     @Override
     public void delete(String uuid) {
-
+        sqlHelper.runSQL("DELETE FROM resume WHERE uuid = ?", pst -> {
+            pst.setString(1, uuid);
+            if (pst.executeUpdate() == 0) {
+                throw new NotExistStorageException(uuid);
+            }
+            return null;
+        });
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        return List.of();
+        return sqlHelper.runSQL("SELECT * FROM resume r ORDER BY full_name, uuid", pst -> {
+            ResultSet rs = pst.executeQuery();
+            List<Resume> allResumes = new ArrayList<>();
+            while (rs.next()){
+                allResumes.add(Resume.of(rs.getString("uuid"), rs.getString("full_name")));
+            }
+            return allResumes;
+        });
     }
 
     @Override
     public int size() {
-        return 0;
+        return sqlHelper.runSQL("SELECT count(*) FROM  resume", pst -> {
+        ResultSet rs = pst.executeQuery();
+        return rs.next() ? rs.getInt(1) : 0;
+        });
     }
 }
